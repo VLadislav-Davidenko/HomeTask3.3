348e9dba72b52161d4bd561da7f34e2036716df5
Visl <5vladik068@gmail.com>
2021-10-15 21:52:47 +0300

    HomeTask 3.3

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 2147483647;
+    float b = (float)a;
+    System.out.println("1) " + a);
+    System.out.println("2) " + b);
+    }
+}
